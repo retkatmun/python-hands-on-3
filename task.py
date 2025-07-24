4099409c8c2fecf5439ccceddc1847d8f418530d
Retkatmun <retkatmun1@gmail.com>
2025-07-24 14:55:30 +0000

    task1 for python hands on

---
+++ task.py
@@ -0,0 +1,13 @@
+meals = ["Gwote", "Masa", "Tuwon Acha", "Fura da Nono", "Kunu", "Miyan Kuka"]
+
+meals.insert(4,"Miyan Taushe")
+print(meals)
+
+meals.remove(meals[1])
+print(meals)
+
+meals.append("Fura Da NOno")
+print(meals)
+
+meals.sort(meals)
+print(meals)
